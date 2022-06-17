5e3b4a02a81bf2cacbee1bb82536a9dc70693b7e
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-06-17 19:15:49 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+
+// configuration object is already inside the fetch
+    return fetch("http://localhost:3000/users", {
+    method : "POST",
+    headers : {
+      "Content-Type" : "application/json",
+      Accept : "application/json"
+    },
+    body : JSON.stringify({
+      name, 
+      email,
+    }),
+  }) 
+
+  //handling with .then()
+  .then(function(response){
+    return response.json()
+  })
+  .then(function(object){
+    document.querySelector('body').innerHTML = object['id']
+  })
+
+  //when things go wrong 
+  .catch(function(error){
+    document.querySelector('body').textContent = error.message
+  })
+}
